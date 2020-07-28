57578103a103d1fcac3493d85be09961e799b0a4
MohamedEssaadani <essaadan80@gmail.com>
2020-07-28 20:29:42 +0100

    visites_and_customers

---
+++ database/migrations/2020_07_28_191531_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->string('matricule');
+            $table->integer('visite_type');
+            $table->foreign('visite_type')
+            ->references('id')
+            ->on('visites');
+            $table->string('full_name');
+            $table->string('phone');
+            $table->string('car_brand');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
