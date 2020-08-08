11a4706f77b739bf899f9893093d2583884241ac
MohamedEssaadani <essaadan80@gmail.com>
2020-08-08 16:46:07 +0100

    commit

--- database/migrations/2020_07_26_202859_add_user_type_to_users_table.php
+++ database/migrations/2020_07_26_202859_add_user_type_to_users_table.php
@@ -15,6 +15,7 @@ class AddUserTypeToUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('userType')->default('user')->after('email');
+            // ->change()
         });
     }
 
